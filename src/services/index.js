8a274c06f274adf319e029e4408a24a106206aec
Nicholas Bour <bournx@dukes.jmu.edu>
2021-11-16 12:06:08 -0500

    setup models

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,9 @@
 const students = require('./students/students.service.js');
+const courses = require('./courses/courses.service.js');
+const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(students);
+  app.configure(courses);
+  app.configure(messages);
 };
